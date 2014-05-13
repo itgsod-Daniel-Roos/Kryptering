bfbef280df0827a59d215cf020efc091008edb72
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-13 10:05:18 +0200

    Added alphabet

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,7 +1,9 @@
+alphabet[] = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
 def encrypt(cleartext, offset):
     if cleartext == '':
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
+    
+
     return ''
-    pass
