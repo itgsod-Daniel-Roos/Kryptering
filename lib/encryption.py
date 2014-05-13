3420c7e7f0f6fcfe53cd427036a7e5f86cd32bef
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-13 11:30:44 +0200

    Made string into uppercase. Uppgiften klar.
    CASE CLOSED.

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,18 +1,19 @@
 
 def encrypt(cleartext, offset):
-    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     if cleartext == '':
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
     encrypted = ""
+    cleartext = cleartext.upper()
     for char in cleartext:
         if char == " ":
             encrypted += char
         elif (alphabet.index(char) + offset) > 25:
             encrypted += alphabet[(alphabet.index(char) + offset - 26)]
         elif (alphabet.index(char) + offset) < 0:
-            encrypted += alphabet[(alphabet.index(char) + offset + 24)]
+            encrypted += alphabet[(alphabet.index(char) + offset + 26)]
         else:
             encrypted += alphabet[(alphabet.index(char) + offset)]
     return encrypted
