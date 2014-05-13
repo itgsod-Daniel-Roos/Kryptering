04cdac1f4534206d3fc20a3f54d093b4609b77ca
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-13 09:35:44 +0200

    test 3 passed

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,4 +1,6 @@
-def encrypt(cleartext, cypher):
+def encrypt(cleartext, offset):
     if cleartext == '':
         raise ValueError('can not encrypt empty string')
+    if offset == 0:
+        raise ValueError('offset must not be zero')
     pass
