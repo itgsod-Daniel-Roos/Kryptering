7aa53e40a85f23c0659d4057d295050a185fc0fd
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-13 09:44:02 +0200

    test 4 passed
    Will have to be revised...

--- lib/encryption.py
+++ lib/encryption.py
@@ -3,4 +3,5 @@ def encrypt(cleartext, offset):
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
+    return ''
     pass
