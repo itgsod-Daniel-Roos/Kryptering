5aae11e2ed15e5ddf5732c796ef7e34b598ac79f
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-13 10:39:43 +0200

    Added letters -> numbers
    May need to be revised

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,9 +1,11 @@
-alphabet[] = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
 def encrypt(cleartext, offset):
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     if cleartext == '':
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
-    
+    for i in cleartext:
+       cleartext[i] = alphabet.index(cleartext[i])
 
     return ''
