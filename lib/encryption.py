319587846b43a66841fc79de088e9f3557a7e0f2
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2014-05-14 11:58:02 +0200

    Added first decrypt + further clean-up

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,6 +1,6 @@
 
 def encrypt(cleartext, offset):
-    alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     if cleartext == '':
         raise ValueError('can not encrypt empty string')
     if offset == 0:
@@ -17,3 +17,23 @@ def encrypt(cleartext, offset):
         else:
             encrypted += alphabet[(alphabet.index(char) + offset)]
     return encrypted
+
+
+def decrypt(encrypt, offset):
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    if encrypt == '':
+        raise ValueError('can not decrypt empty string')
+    if offset == 0:
+        raise ValueError('offset must not be zero')
+    decrypted = ""
+    encrypt = encrypt.upper()
+    for char in encrypt:
+        if char == " ":
+            decrypted += char
+        elif (alphabet.index(char) + offset) > 25:
+            decrypted += alphabet[(alphabet.index(char) + offset - 26)]
+        elif (alphabet.index(char) + offset) < 0:
+            decrypted += alphabet[(alphabet.index(char) + offset + 26)]
+        else:
+            decrypted += alphabet[(alphabet.index(char) + offset)]
+    return decrypted
